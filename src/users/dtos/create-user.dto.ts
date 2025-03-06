863eecd7e0d8d2d38fb860c585971f32a9785de6
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2025-03-06 14:47:34 +0200

    Nest validation with pipes, class validator, and class transformer

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,6 +1,22 @@
+import { IsEmail, IsString, Length } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @Length(3, 20)
+  // @Length(3, 20, {
+  //   message: 'Incorrect Length, username must between 3 and 20 characters',
+  //   groups: ['create'],
+  // })
+  // @Length(6, 30, {
+  //   message: 'Incorrect Length, username must between 6 and 30 characters',
+  //   groups: ['update'],
+  // })
   readonly username: string;
+
+  @IsEmail({}, { message: 'Incorrect email' })
   readonly email: string;
+
+  @IsString()
   readonly country: string;
 
   // readonly mean no business logic here In CreateUserDto
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,10 +7,14 @@ import {
   HttpStatus,
   NotFoundException,
   Param,
+  ParseIntPipe,
+  ParseUUIDPipe,
   Patch,
   Post,
   Req,
   Res,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { CreateUserDto } from './dtos/create-user.dto';
@@ -19,6 +23,7 @@ import { UserEntity } from './user.entity';
 import { v4 as uuid } from 'uuid';
 
 @Controller('users')
+// @UsePipes(ValidationPipe)
 export class UsersController {
   private users: UserEntity[] = [];
 
@@ -30,7 +35,10 @@ export class UsersController {
 
   @Get(':id')
   @HttpCode(HttpStatus.OK)
-  findOne(@Param('id') id: string): UserEntity {
+  findOne(
+    @Param('id', ParseUUIDPipe)
+    id: string,
+  ): UserEntity {
     const user = this.users.find((user) => user.id === id);
     if (!user) {
       throw new NotFoundException(`User with id ${id} not found`);
@@ -45,9 +53,28 @@ export class UsersController {
   //   return 'Create User';
   // }
 
+  // @Post()
+  // @HttpCode(HttpStatus.CREATED)
+  // createUser(
+  //   @Body(ValidationPipe) createUserDto: CreateUserDto,
+  // ): CreateUserDto {
+  //   const newUser: UserEntity = {
+  //     ...createUserDto,
+  //     id: uuid(),
+  //   };
+
+  //   this.users.push(newUser);
+
+  //   return newUser;
+  // }
+
   @Post()
+  // @UsePipes(new ValidationPipe({ groups: ['create'] }))
   @HttpCode(HttpStatus.CREATED)
-  createUser(@Body() createUserDto: CreateUserDto): CreateUserDto {
+  createUser(
+    @Body()
+    createUserDto: CreateUserDto,
+  ): CreateUserDto {
     const newUser: UserEntity = {
       ...createUserDto,
       id: uuid(),
@@ -61,10 +88,12 @@ export class UsersController {
   // Patch for update specific field in object
   // Put for update all object (To update specific field must send full object)
   @Patch(':id')
+  // @UsePipes(new ValidationPipe({ groups: ['update'] }))
   @HttpCode(HttpStatus.OK)
   update(
-    @Param('id') id: string,
-    @Body() updateUserDto: UpdateUserDto,
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body()
+    updateUserDto: UpdateUserDto,
   ): UpdateUserDto {
     // 1) find element index that we want to update
     const index = this.users.findIndex((user) => user.id === id);
@@ -82,7 +111,7 @@ export class UsersController {
 
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
-  remove(@Param('id') id: string): { message: string } {
+  remove(@Param('id', ParseUUIDPipe) id: string): { message: string } {
     const initialLength = this.users.length;
 
     this.users = this.users.filter((user) => user.id !== id);
